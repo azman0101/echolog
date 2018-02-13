06c5c1ea224b075e4ddfd9b261ce5e19b67da035
azman0101 <julien.boulanger@oxalide.com>
2018-02-13 14:35:36 +0100

    Increase verbosity

--- echo.sh
+++ echo.sh
@@ -10,7 +10,20 @@ trap cleanup SIGINT SIGTERM
 echo $SPEED
 while [ 1 ]
 do
-	echo "Loggggginnnnnggg !!!! $(date)"
+	echo "Loggggginnnnnggg LoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnnggg !!!! $(date)"
+	echo "Loggggginnnnnggg LoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnnggg !!!! $(date)"
+	echo "Loggggginnnnnggg LoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnnggg !!!! $(date)"
+	echo "Loggggginnnnnggg LoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnnggg !!!! $(date)"
+	echo "Loggggginnnnnggg LoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnnggg !!!! $(date)"
+	echo "Loggggginnnnnggg LoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnnggg !!!! $(date)"
+	echo "Loggggginnnnnggg LoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnnggg !!!! $(date)"
+	echo "Loggggginnnnnggg LoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnnggg !!!! $(date)"
+	echo "Loggggginnnnnggg LoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnnggg !!!! $(date)"
+	echo "Loggggginnnnnggg LoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnnggg !!!! $(date)"
+	echo "Loggggginnnnnggg LoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnnggg !!!! $(date)"
+	echo "Loggggginnnnnggg LoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnnggg !!!! $(date)"
+	echo "Loggggginnnnnggg LoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnnggg !!!! $(date)"
+	echo "Loggggginnnnnggg LoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnngggLoggggginnnnnggg !!!! $(date)"
 	sleep $SPEED &
 	wait $!
 done
